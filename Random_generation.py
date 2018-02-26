55747da817f0930c9021034ce7a75b540c2ea3c2
Nikhesh Muralidharan <nikekailash@gmail.com>
2018-02-26 18:32:05 +0530

    Add files via upload

--- Random_generation.py
+++ Random_generation.py
@@ -1,11 +1,6 @@
-import numpy as np
-import math
 import random
 x = 0
-lis=[]
 while x < 100:
-    ra = random.uniform(-1.0,1.0)
-    r=2*(-1*np.log(ra))**(2)
-    lis.append(r)
+    ran = random.uniform(-1.0,1.0)
+    print "The numbere is " ,ran
     x+=1
-print r
